4c10830ab57c3b7a33e7a7bd5a480a124123c7cd
mharoonb <mharoonb@users.noreply.github.com>
2024-11-05 19:25:18 +0300

    Fix: Update build configuration to install react-scripts globally

--- build.sh
+++ build.sh
@@ -5,5 +5,11 @@ set -o errexit
 # Install dependencies
 npm install
 
+# Install react-scripts globally
+npm install -g react-scripts
+
+# Set CI to false
+export CI=false
+
 # Build the React app
 npm run build
